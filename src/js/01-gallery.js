9fa30cdbe8828dccae10640c0875aeee0d71a3e0
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2022-12-21 21:54:38 +0200

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+         </a>
+    </div>`
+    )
+    .join("");
+}
+
+new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
 
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+email.value = data.email || '';
+message.value = data.message || '';
+
+function onFormInput(event) {
+    if (data) {
+    formData = data;
+  }
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+    
+ function onFormSubmit(event) {
+     event.preventDefault();
+ const {
+        elements: { email, message }
+      } = event.target;
+      
+      if (email.value === "" || message.value === "") {
+        return window.alert("Please, fill in all the fields!");
+      };
+    // console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    
+};
+   
